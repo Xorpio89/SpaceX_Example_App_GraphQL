8778738dcdd59b23ecc86a65916e92b815d86d21
Oliver Wach <git@abramov-software.de>
2018-11-13 13:13:44 +0100

    Finished first version of the Frontend application

---
+++ public/precache-manifest.0ac3889b61ab52776f47c57ede6c0877.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37a39b35e699355acde6016e5ce69039",
+    "url": "/static/media/logo.37a39b35.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "790c8f3df4ce48392181",
+    "url": "/static/js/main.790c8f3d.chunk.js"
+  },
+  {
+    "revision": "7e8a3721b761712eb78f",
+    "url": "/static/js/1.7e8a3721.chunk.js"
+  },
+  {
+    "revision": "790c8f3df4ce48392181",
+    "url": "/static/css/main.cc28382b.chunk.css"
+  },
+  {
+    "revision": "31994483790c690a1a1893aba1cbd717",
+    "url": "/index.html"
+  }
+];
